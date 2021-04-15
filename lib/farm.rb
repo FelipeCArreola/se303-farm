e43f2579d4597b82f3365512bad5418a83783953
Felipe Arreola <arreolaf@oregonstate.edu>
2021-04-15 10:35:47 -0700

    passed first test

--- lib/farm.rb
+++ lib/farm.rb
@@ -1,8 +1,15 @@
-class farm
+class Farm
 
   def initialize(_) 
     @array = _
   end
 
+  def verse(_)
+    "Old MacDonald had a farm, E-I-E-I-O,"              + "\n" +
+    "And on that farm he had a cow, E-I-E-I-O,"         + "\n" +
+    "With an moo moo here and an moo moo there,"        + "\n" +
+    "Here an moo, there an moo, everywhere an moo moo," + "\n" +
+    "Old MacDonald had a farm, E-I-E-I-O."
+  end
 
 end
