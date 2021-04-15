828b688b3b72d14774217bb5238ba2b5445caa2b
Felipe Arreola <arreolaf@oregonstate.edu>
2021-04-15 10:48:23 -0700

    passed second test-

--- lib/farm.rb
+++ lib/farm.rb
@@ -4,12 +4,21 @@ class Farm
     @array = _
   end
 
-  def verse(_)
-    "Old MacDonald had a farm, E-I-E-I-O,"              + "\n" +
-    "And on that farm he had a cow, E-I-E-I-O,"         + "\n" +
-    "With an moo moo here and an moo moo there,"        + "\n" +
-    "Here an moo, there an moo, everywhere an moo moo," + "\n" +
-    "Old MacDonald had a farm, E-I-E-I-O."
+  def verse(animal = nil)
+    case animal.species
+    when 'cow'  
+      "Old MacDonald had a farm, E-I-E-I-O,"                  + "\n" +
+      "And on that farm he had a cow, E-I-E-I-O,"             + "\n" +
+      "With an moo moo here and an moo moo there,"            + "\n" +
+      "Here an moo, there an moo, everywhere an moo moo,"     + "\n" +
+      "Old MacDonald had a farm, E-I-E-I-O."
+    when 'pig'
+      "Old MacDonald had a farm, E-I-E-I-O,"                  + "\n" +
+      "And on that farm he had a pig, E-I-E-I-O,"             + "\n" +
+      "With an oink oink here and an oink oink there,"        + "\n" +
+      "Here an oink, there an oink, everywhere an oink oink," + "\n" +
+      "Old MacDonald had a farm, E-I-E-I-O."  
+    end
   end
 
 end
